eb8f35d7addd23d78d7f9786c2ce24bfbefebbc1
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-08 15:01:45 -0300

    Initial Commit

---
+++ BMOAulasCpp18VirtualFunctionsInCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BMOAulasCpp18VirtualFunctionsInCpp
+//
+//  Created by Breno Medeiros on 08/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
